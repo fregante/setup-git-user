9a7387308fa6f6d01638cb78e911bea7a6682ebd
Federico Brigante <opensource@bfred.it>
2019-09-26 00:53:55 +0700

    Add action

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
-const core = require('@actions/core');
-const wait = require('./wait');
+'use strict';
+const fs = require('fs');
+const path = require('path');
 
-
-// most @actions toolkit packages have async methods
-async function run() {
-  try { 
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
-
-    core.debug((new Date()).toTimeString())
-    wait(parseInt(ms));
-    core.debug((new Date()).toTimeString())
-
-    core.setOutput('time', new Date().toTimeString());
-  } 
-  catch (error) {
-    core.setFailed(error.message);
-  }
+if (!process.env.INPUT_TOKEN) {
+	throw new Error('Input required and not supplied: token');
 }
 
-run()
+const throwErrors = err => {
+	if (err) {
+		throw err;
+	}
+};
+
+fs.appendFile('.git/config', `
+[user]
+	name = ${process.env.INPUT_NAME}
+	email = ${process.env.INPUT_EMAIL}
+`, throwErrors);
+
+fs.writeFile(path.join(process.env.HOME, '.netrc'), `
+machine github.com
+login ${process.env.GITHUB_REPOSITORY.replace(/\/.+/, '')}
+password ${process.env.INPUT_TOKEN}
+`, throwErrors);
