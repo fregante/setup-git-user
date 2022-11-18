afcb18cd46645b7569b525b1fa25337be0870222
Marco Casaroli <marco.casaroli@gmail.com>
2022-11-18 10:00:05 +0100

    Create `.git` directory if it does not exist

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 
-require('fs').appendFile('.git/config', `
+const fs = require("fs");
+fs.existsSync(".gitaa") || fs.mkdirSync(".gitaa");
+fs.appendFile('.git/config', `
 [user]
 	name = github-actions[bot]
 	email = 41898282+github-actions[bot]@users.noreply.github.com
