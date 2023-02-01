15a6391dc3dd32c8d521727fa9760f72d6d74868
Federico Brigante <me@fregante.com>
2023-02-01 12:48:09 +0800

    use actor

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 'use strict';
 
+const user = process.env.GITHUB_ACTOR ?? 'github-actions';
+const userEmail = process.env.GITHUB_ACTOR ?? '41898282+github-actions[bot]';
+
+
 require('fs').appendFile('.git/config', `
 [user]
-	name = github-actions[bot]
-	email = 41898282+github-actions[bot]@users.noreply.github.com
+	name = ${user}
+	email = ${userEmail}
 `, error => {
 	if (error) {
 		throw error;
